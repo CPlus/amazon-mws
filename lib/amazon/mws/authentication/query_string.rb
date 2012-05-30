914a539ea9f4dc57f039e5765b04d1c6012657e5
Alexandru Stubbs <alex.stubbs@dachisgroup.com>
2012-05-30 13:09:09 +0100

    Marketplace seems to be submitted as a different parameter nowadays

--- lib/amazon/mws/authentication/query_string.rb
+++ lib/amazon/mws/authentication/query_string.rb
@@ -4,7 +4,7 @@ class Amazon::MWS::Authentication
     def initialize(params = {})
       query_params = {
         'AWSAccessKeyId'   => params[:access_key],
-        'Marketplace'      => params[:marketplace_id],
+        'MarketplaceIdList.Id.1'      => params[:marketplace_id],
         'SignatureMethod'  => Signature::METHOD,
         'SignatureVersion' => Signature::VERSION,
         'Timestamp'        => Time.now.iso8601
